ed4911920ab8a71e46f79ae0656e3dabe4de3da2
jmcadam <mcadam88@gmail.com>
2020-11-23 17:11:38 +0000

    Update ETOnline_CreateCase.scala

--- src/gatling/simulations/uk/gov/hmcts/ethos/scenario/ETOnline_CreateCase.scala
+++ src/gatling/simulations/uk/gov/hmcts/ethos/scenario/ETOnline_CreateCase.scala
@@ -12,7 +12,7 @@ object ETOnline_CreateCase {
 
     def createSingleuUID: String = randomUUID.toString
     def createMultipleuUID: String = randomUUID.toString
-    def dateTime = "PT9"
+    def dateTime = "PT10"
     val feedMultiCaseValue = csv("EthosMultiCaseRef.csv")
 
     val ETOnline_CreateSingle = 
